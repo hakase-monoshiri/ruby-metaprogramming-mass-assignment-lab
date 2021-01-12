fde0fb3b53beed631c09cd7bae4e0df62c22a014
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-12 12:17:56 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  #your code here
+  
+def initialize(keyword_hash)
+  keyword_hash.each do |key, value|
+    self.class.attr_accessor(key)
+    self.send(("#{key}="), value)
+  end
+end
+
 end
